9d9e197a1dd2a7d86e25e1963fff226c45046a71
leonsky0407@gmail.com <leonsky0407@gmail.com>
2021-02-03 13:31:53 -0300

    aula inicial

---
+++ src/main/java/com/example/apiTest/ApiTestApplication.java
@@ -0,0 +1,19 @@
+package com.example.apiTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestApplication.class, args);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/apiTest/controller/ApiController.java
@@ -0,0 +1,22 @@
+package com.example.apiTest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class ApiController {
+	
+
+	@GetMapping("/")
+	public ResponseEntity<Object> getHelloWorld(){
+		log.info("testando lombok");
+		return new ResponseEntity<>("ola mundo",HttpStatus.OK);
+	}
+	
+
+}
